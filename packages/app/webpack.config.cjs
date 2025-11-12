4e61125a74c1c5b39d3fb7f8eb4a0b69df41f94e
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2025-11-12 22:06:39 +0100

    Setup app build

---
+++ packages/app/webpack.config.cjs
@@ -0,0 +1,30 @@
+/* eslint-disable */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+  devServer: {
+    static: './build',
+    open: true,
+  },
+};
--- packages/digital-clock/rollup.config.js
+++ packages/digital-clock/rollup.config.js
@@ -1,7 +1,7 @@
 import terser from '@rollup/plugin-terser';
 import bundleSize from 'rollup-plugin-bundle-size';
 import typescript from 'rollup-plugin-typescript2';
-import jsx from 'rollup-plugin-jsx'
+import jsx from 'rollup-plugin-jsx';
 
 import pkg from './package.json' with { type: "json" };
 
@@ -10,7 +10,7 @@ export default [
     input: 'src/index.ts',
     output: [{ file: pkg.module, format: 'esm' }],
     plugins: [
-      typescript({cacheRoot: '.cache'}),
+      typescript({ cacheRoot: '.cache' }),
       jsx({ factory: 'React.createElement' }),
       terser(),
       bundleSize(),
