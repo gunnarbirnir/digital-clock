f9a3d18b16841e5484a54c5688622a21abbd64b5
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2025-11-13 19:44:20 +0100

    Add component as dependency in app

--- packages/app/src/index.tsx
+++ packages/app/src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import DigitalClock from '@gunnarbirnir/digital-clock';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
     <h1>Digital Clock App</h1>
+    <DigitalClock />
   </React.StrictMode>
 );
