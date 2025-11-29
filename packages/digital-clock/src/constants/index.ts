4394f3fe2eab4dd4c03a1edc8a148a39a2e075ab
Gunnar Birnir Ólafsson <gunnarbirnir@gmail.com>
2025-11-29 13:38:51 +0100

    Add jenkinsfile

--- packages/digital-clock/src/constants/index.ts
+++ packages/digital-clock/src/constants/index.ts
@@ -3,8 +3,8 @@ import type { NumberValue } from '../types';
 export const DEFAULT_NUMBER_HEIGHT = 200;
 export const DEFAULT_NUMBER_THICKNESS = 20;
 export const DEFAULT_NUMBER_INSET = 1;
-export const DEFAULT_ACTIVE_COLOR = 'hotpink';
-export const DEFAULT_INACTIVE_COLOR = '#333333';
+export const DEFAULT_ACTIVE_COLOR = '#FF7F50';
+export const DEFAULT_INACTIVE_COLOR = '#444444';
 
 export const NUMBER_VALUES: Record<NumberValue, number[]> = {
   '0': [1, 1, 1, 0, 1, 1, 1],
